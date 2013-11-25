2e8a97d10af65d45dbca4884740871e143bcf506
Nikita Grishin <mgrin@MacBook-Pro-Nikita.local>
2013-11-25 19:07:04 +0100

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var routes = require('./routes.js');
+
+app.configure(function () {
+    app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
+    app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
+    app.use(express.static('public'));
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: 'secret epfl session'
+    }));
+    app.use(express.bodyParser());
+//    app.use(passport.initialize());
+//    app.use(passport.session());
+    app.use(app.router);
+});
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+
+app.get('/', routes.index);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
