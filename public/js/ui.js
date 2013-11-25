e2374659099e37c0ba3ff22abdf7c3db7b316659
Nikita Grishin <mgrin@MacBook-Pro-Nikita.local>
2013-11-25 20:18:48 +0100

    responsive stuff

--- public/js/ui.js
+++ public/js/ui.js
@@ -2,21 +2,23 @@ var defPadding = 10;
 
 function initiate() {
     console.log('Initiation of UI');
-    
+
     console.log('Setting up all .bottom-aligned');
-    $('.bottom-aligned').each(function () {
-        $(this).css('margin-top', $('.header').height() - $(this).height() - defPadding)
-    });
-    
+    if ($(window).width() > 970) {
+        $('.bottom-aligned').each(function () {
+            $(this).css('margin-top', $('.header').height() - $(this).height() - defPadding)
+        });
+    }
+
     console.log('Setting up all .user-space-support');
     $('.user-space-support').each(function () {
         $(this).css('height', $(this).parent().height())
     });
-    
+
     console.log('Setting up .footer');
-    
-    $('.footer').each(function(){
+
+    $('.footer').each(function () {
         $(this).css('height', 1.5 * $('.header').height());
-        $(this).css('margin-top', $(document).height()-3.3*$(this).height());
+        $(this).css('margin-top', $(document).height() - 3.3 * $(this).height());
     });
 }
