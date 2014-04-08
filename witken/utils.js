d95fe318390d51e80a107b7fd8396e9a817a3464
MGrin <mr6r1n@gmail.com>
2014-04-08 19:10:03 +0200

    Add console.log in error creation

--- witken/utils.js
+++ witken/utils.js
@@ -3,12 +3,16 @@ var ClientError = function (field, message) {
     this._type = 'client';
     this.field = field;
     this.message = message;
+
+    console.log('Client error in '+field+' field: '+message);
 }
 
 var ServerError = function (message) {
     this.content = 'Error';
     this._type = 'server';
     this.message = message;
+
+    console.log('Server error: '+message);
 }
 
 var DatabaseError = function (database, error) {
@@ -16,6 +20,7 @@ var DatabaseError = function (database, error) {
     this._type = 'database';
     this.database = database;
     this.error = error;
+    console.log('Database error in '+database+': '+error);
 }
 
 exports.ClientError = ClientError;
